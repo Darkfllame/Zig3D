29cf8d7db7a55c3efca30894570c15887f448e43
Darkfllame <ntmconar5@gmail.com>
2024-03-15 02:36:56 +0100

    changed image definition a bit to include channels, also forced files to load with 4 channels (should i remove the channels field ?)

--- src/stb.zig
+++ src/stb.zig
@@ -3,16 +3,24 @@ const utils = @import("utils");
 const glfw = @import("glfw");
 const image = @import("image");
 const c = @cImport({
-    @cDefine("STB_IMAGE_IMPLEMENTATION", "");
     @cInclude("STB/stb_image.h");
+    @cInclude("string.h");
 });
 
 const Allocator = std.mem.Allocator;
+
+pub fn setFlipVerticallyOnLoad(b: bool) void {
+    c.stbi_set_flip_vertically_on_load(@intFromBool(b));
+}
+pub fn setFlipVerticallyOnLoadThread(b: bool) void {
+    c.stbi_set_flip_vertically_on_load_thread(@intFromBool(b));
+}
 
 pub const Image = struct {
     pixels: []u8,
     width: u32,
     height: u32,
+    channels: u32,
 
     pub fn load(allocator: Allocator, filename: []const u8, errStr: ?*[]const u8) Allocator.Error!?Image {
         const filenameZ = utils.copy(
@@ -21,15 +29,26 @@ pub const Image = struct {
             try allocator.alloc(u8, filename.len + 1),
         );
         defer allocator.free(filenameZ);
+        filenameZ[filename.len] = 0;
 
-        return loadZ(filename, errStr);
+        return loadZ(filenameZ[0..filename.len :0], errStr);
     }
     pub fn loadZ(filename: [:0]const u8, errStr: ?*[]const u8) ?Image {
         var x: c_int = 0;
         var y: c_int = 0;
         var channels: c_int = 0;
-        const pixels: [*c]u8 = c.stbi_load(filename, &x, &y, &channels, 0) orelse {
-            if (errStr != null) errStr.?.* = c.stbi_failure_reason();
+        const pixels: [*c]u8 = c.stbi_load(
+            filename,
+            &x,
+            &y,
+            &channels,
+            4,
+        ) orelse {
+            if (errStr) |es| {
+                const reason = c.stbi_failure_reason();
+                const len = c.strlen(reason);
+                es.* = reason[0..len];
+            }
             return null;
         };
 
@@ -37,10 +56,38 @@ pub const Image = struct {
             .pixels = pixels[0..@intCast(x * y)],
             .width = @intCast(x),
             .height = @intCast(y),
+            .channels = @intCast(channels),
+        };
+    }
+    pub fn loadFromMemory(buffer: []const u8, errStr: ?*[]const u8) ?Image {
+        var x: c_int = 0;
+        var y: c_int = 0;
+        var channels: c_int = 0;
+        const pixels: [*c]u8 = c.stbi_load_from_memory(
+            @ptrCast(buffer.ptr),
+            @intCast(buffer.len),
+            &x,
+            &y,
+            &channels,
+            4,
+        ) orelse {
+            if (errStr) |es| {
+                const reason = c.stbi_failure_reason();
+                const len = c.strlen(reason);
+                es.* = reason[0..len];
+            }
+            return null;
+        };
+
+        return .{
+            .pixels = pixels[0..@intCast(x * y)],
+            .width = @intCast(x),
+            .height = @intCast(y),
+            .channels = @intCast(channels),
         };
     }
     pub fn delete(self: *Image) void {
-        c.stbi_image_free(self.pixels);
+        c.stbi_image_free(@ptrCast(self.pixels.ptr));
         self.* = undefined;
     }
 
