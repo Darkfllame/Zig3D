d0c6c577473e5ec60bf1822ffee2d33927448935
Darkfllame <ntmconar5@gmail.com>
2024-06-11 15:11:26 +0200

    removed autodoc

---
+++ src/DGR/lib.zig
@@ -0,0 +1,11 @@
+//! DGR (Dagger) is a cross-platform 2d (and 3d in the future)
+//! game engine.
+//!
+//! have fun using it. (homemade btw)
+//!
+
+const std = @import("std");
+const builtin = @import("builtin");
+
+const glad = @import("glad");
+const glfw = @import("glfw");
